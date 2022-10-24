4a77f29995e57339d6ac075270596fc00cd0143b
GhislainAdon <esaticsrit1b@gmail.com>
2022-10-24 13:20:02 -0700

    0x05 Pointer array string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+/*
+ * Project : 0x05 .
+ * Programmer: @Deep Down Craft
+ * For ALx School 2022
+ */
+
+#include "main.h"
+/**
+ * print_rev - Prints a string in reverse.
+ * @s: string to be reserved.
+ */
+void print_rev(char *s)
+{
+	int len = 0, i;
+
+	while (s[i++])
+		len++;
+
+	for (i = len - 1; i >= 0; i--)
+		_putchar(s[i]);
+
+	_putchar('\n');
+}
+
---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+/*
+ * Project : 0x05 .
+ * Programmer: @Deep Down Craft
+ * For ALx School 2022
+ */
+
+#include "main.h"
+/**
+ * rev_string - Reverses a string.
+ * @s: string to be reserved.
+ */
+void rev_string(char *s)
+{
+	int i = 0, len = 0;
+	char tmp;
+
+	while (s[i++])
+		len++;
+
+	for (i = len - 1; i >= len / 2; i--)
+	{
+		tmp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = tmp;
+	}
+}
